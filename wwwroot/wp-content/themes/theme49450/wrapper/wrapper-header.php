02072b19390416ab15ad8af80104acb87b023f13
yang <hyan0079@student.monash.edu>
2022-05-23 12:42:29 +1000

    v3.1

--- wwwroot/wp-content/themes/theme49450/wrapper/wrapper-header.php
+++ wwwroot/wp-content/themes/theme49450/wrapper/wrapper-header.php
@@ -1,14 +1,11 @@
 <?php /* Wrapper Name: Header */ ?>
 <div class="row">
-	<div class="span6" data-motopress-type="static" data-motopress-static-file="static/static-logo.php">
+	<div class="span5" data-motopress-type="static" data-motopress-static-file="static/static-logo.php">
 		<?php get_template_part("static/static-logo"); ?>
 	</div>
-	<div class="span6 header-sidebar" data-motopress-type="dynamic-sidebar" data-motopress-sidebar-id="footer-sidebar-4">
-		<?php dynamic_sidebar("header-sidebar"); ?>
-	</div>
-</div>
-<div class="row nav-top">
-	<div class="span12" data-motopress-type="static" data-motopress-static-file="static/static-nav.php">
-		<?php get_template_part("static/static-nav"); ?>
-	</div>
+        <div class="span7 row nav-top">
+        <div class="span12" data-motopress-type="static" data-motopress-static-file="static/static-nav.php">
+            <?php get_template_part("static/static-nav"); ?>
+        </div>
+    </div>
 </div>
